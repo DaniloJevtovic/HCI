9acec7c03fe0f23492af1fa1a33ca832253e2e71
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2017-04-12 12:37:24 +0200

    zapoceta serijalizacija etikete

--- Projekat/Projekat/Etiketa/Etiketa.cs
+++ Projekat/Projekat/Etiketa/Etiketa.cs
@@ -2,13 +2,94 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace Projekat.Etiketa
 {
-    public class Etiketa
+    [XmlRoot("Etiketa")]
+    //[Serializable]
+    public class Etiketa: INotifyPropertyChanged
     {
-        public string Oznaka { get; set; }
-        public string Boja { get; set; }
-        public string Opis { get; set; }
+        private string _oznaka;
+        private string _boja;
+        private string _opis;
+        private List<Etiketa> etikete;
+
+        public string Oznaka 
+        {
+            get
+            {
+                return _oznaka;
+            }
+            set
+            {
+                if (value != _oznaka)
+                {
+                    _oznaka = value;
+                    OnPropertyChanged("Oznaka");
+                }
+            }
+        }
+
+        public string Boja 
+        {
+            get
+            {
+                return _boja;
+            }
+
+            set
+            {
+                if (value != _boja)
+                {
+                    _boja = value;
+                    OnPropertyChanged("Boja");
+                }
+            }  
+        }
+
+        public string Opis
+        {
+            get
+            {
+                return _opis;
+            }
+
+            set
+            {
+                if (value != _opis)
+                {
+                    _opis = value;
+                    OnPropertyChanged("Opis");
+                }
+            }
+        }
+
+        public List<Etiketa> Etikete
+        {
+            get
+            {
+                return etikete;
+            }
+
+            set
+            {
+                if (value != etikete)
+                {
+                    etikete = value;
+                    OnPropertyChanged("Etikete");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
--- Projekat/Projekat/Etiketa/NovaEtiketa1.xaml.cs
+++ Projekat/Projekat/Etiketa/NovaEtiketa1.xaml.cs
@@ -10,23 +10,112 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Serialization;
 
 using Projekat.Help;
+using System.ComponentModel;
+
+
 
 namespace Projekat.Etiketa
 {
     /// <summary>
     /// Interaction logic for NovaEtiketa1.xaml
     /// </summary>
-    public partial class NovaEtiketa1 : Window
+    public partial class NovaEtiketa1 : Window, INotifyPropertyChanged
     {
+        public Etiketa etiketa;
+        [XmlArray("Etikete"), XmlArrayItem(typeof(Etiketa), ElementName = "Etiketaaa")]
+        public List<Etiketa> etikete;
+        
         public NovaEtiketa1()
         {
             InitializeComponent();
+            etiketa = new Etiketa();
+            etikete = new List<Etiketa>();
+
+            //etikete = ucitajDatoteku("etikete");
+ 
         }
 
+        public void sacuvajDatoteku(String fileName)
+        {
+            using(FileStream stream = new FileStream(fileName, FileMode.Append))
+            {
+                var serializer = new XmlSerializer(typeof(List<Etiketa>));
+                serializer.Serialize(stream, etikete);
+            }
+        }
+
+        public static List<Etiketa> ucitajDatoteku(String fileName)
+        {
+            using(var stream = new FileStream(fileName,FileMode.Open))
+            {
+                var deserializer = new XmlSerializer(typeof(List<Etiketa>));
+                return (List<Etiketa>)deserializer.Deserialize(stream);
+            }
+        }
+
+        public void dodajEtiketu()
+        {
+            etiketa.Oznaka = txtOznaka.Text;
+            etiketa.Boja = cmBoja.Text;
+            etiketa.Opis = txtOpis.Text;
+
+            etikete.Add(etiketa);
+            sacuvajDatoteku("etikete");
+        }
+
+        /*
+        public void dodajEtiketu(Etiketa et)
+        {
+            etiketa.Oznaka = txtOznaka.Text;
+            etiketa.Boja = cmBoja.Text;
+            etiketa.Opis = txtOpis.Text;
+  
+            etikete.Add(et);
+
+            sacuvajDatoteku(etikete);
+        }
+
+        public void sacuvajDatoteku(List<Etiketa> etiketee)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Etiketa>));
+            using (TextWriter writer = new StreamWriter("etikete.xml"))
+            {
+                serializer.Serialize(writer, etiketee);
+            }
+        }
+
+        public void ucitajDatoteku()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<Etiketa>));
+            TextReader reader = new StreamReader("etikete.xml");
+            object obj = deserializer.Deserialize(reader);
+            
+
+        }
+        */
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+
+        #region Click
+
+        
         private void btnPotvrdi_Click(object sender, RoutedEventArgs e)
         {
+            //dodajEtiketu(etiketa);
+            dodajEtiketu();
             this.Close();
         }
 
@@ -40,5 +129,6 @@ namespace Projekat.Etiketa
             var s = new Pomoc();
             s.Show();
         }
+        #endregion
     }
 }
